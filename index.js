820b0ea45b4d818066a2f69ab2d7c916faf39e06
sampathoops <sampath.oops@gmail.com>
2018-07-14 14:05:15 +0530

    index

--- index.js
+++ index.js
@@ -79,19 +79,19 @@ app.get("/set", function(request, response) {
 
 app.get("/get", function(request, response) {
   var REDISCLOUD_URL = 'redis-16431.c10.us-east-1-2.ec2.cloud.redislabs.com:16431';
-  res.send(process.env);
+  // res.send(process.env);
 
 var redisURL = url.parse(process.env.REDISCLOUD_URL);
 var client = redis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true});
 client.auth(redisURL.auth.split(":")[1]);
 // res.send(client);
-  /* client.get("welcome_msg", function (err, reply) {
+  client.get("welcome_msg", function (err, reply) {
     if (reply != null) {
       res.send(reply);
     } else {
       res.send("Error");
     }
-  }); */
+  });
 });
 
 app.get("/redis", function(request, response) {
