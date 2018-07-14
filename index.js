5dcee7f4e965f551d3bf9f9fb5fcaf3aa80d8584
sampathoops <sampath.oops@gmail.com>
2018-07-14 13:50:42 +0530

    index

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ var pages = [];
   });
 
 var REDISCLOUD_URL = 'redis-16431.c10.us-east-1-2.ec2.cloud.redislabs.com:16431';
-var redisURL = url.parse(process.env.REDISCLOUD_URL);
-var client = redis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true});
+var redisURL = url.parse(process.env.HEROKU_REDIS_GREEN_URL);
+var client = redis.createClient(process.env.HEROKU_REDIS_GREEN_URL, {no_ready_check: true});
 client.auth(redisURL.auth.split(":")[1]);
 console.log('--client--', client);
 
