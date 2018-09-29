9fb6cf216dbf29417c97f8f7425f6d637ce90995
sampathoops <sampath.oops@gmail.com>
2018-09-29 13:46:01 +0530

    Day 9

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ app.post('/submitGetSlot', function(req, res) {
         members = req.body.members;
         client.set(email, members);
     //res.send(pages.getQuote);
-    res.send(pages.getSlot);
+    res.redirect('/getSlot');
 });
 
 app.get("/set", function(request, response) {
